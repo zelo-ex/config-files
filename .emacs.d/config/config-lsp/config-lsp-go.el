ad0999b674ac96d2d58977470a514d65b1ee77a1
zal-dev <ljy180418995@outlook.com>
2026-06-06 01:16:04 +0800

    chore(conf): back up .emacs.d files

---
+++ .emacs.d/config/config-lsp/config-lsp-go.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'lsp-go)
+(defun custom-go-mode-hook ()
+  (setq-local indent-tabs-mode t)
+  (add-hook 'before-save-hook #'lsp-format-buffer nil t))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook
+  (go-ts-mode . lsp-deferred)
+  (go-ts-mode . custom-go-mode-hook)
+  :mode ("\\.go\\'" . go-ts-mode)
+  :init
+  (setq lsp-completion-provider :capf)
+  (setq lsp-completion-enable t))
+
+(provide 'config-lsp-go)
