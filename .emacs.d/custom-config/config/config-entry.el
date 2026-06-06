3ff2c1c0df7d6142cfee710454aec76d7a9c7a87
ZELO_EX <ljy180418995@outlook.com>
2026-06-07 01:21:34 +0800

    Delete .emacs.d/custom-config directory
    It repeated this directory again, so i must remove it again.

--- .emacs.d/custom-config/config/config-entry.el
+++
@@ -1,8 +0,0 @@
-(let* ((root (file-name-directory load-file-name)))
-
-  (add-to-list 'load-path (expand-file-name "config-lsp" root))
-  (require 'config-lsp-init)
-  (add-to-list 'load-path (expand-file-name "config-utils" root))
-  (require 'config-utils-themes))
-
-(provide 'config-entry)
