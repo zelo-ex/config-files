7532a4473f916e177be379ad9c838c51d75228f0
zal-dev <ljy180418995@outlook.com>
2026-06-07 12:52:39 +0800

    chore(conf): back up .emacs.d files

--- .emacs.d/kbd.el
+++ .emacs.d/kbd.el
@@ -13,4 +13,26 @@
   (delete-char 1))
 (global-set-key (kbd "C-k") 'delete-line-no-kill)
 
+(defun custom-split-window-and-new-scratch (window-direction)
+  (let ((split-func (if (eq window-direction 'right)
+			#'split-window-right
+		      #'split-window-below)))
+    (funcall split-func))
+  (other-window 1)
+  (let* ((new-buffer-name (generate-new-buffer-name "*scratch*"))
+	 (new-buffer (get-buffer-create new-buffer-name)))
+    (switch-to-buffer new-buffer)
+    (lisp-interaction-mode)
+    (when initial-scratch-message
+      (insert (substitute-command-keys initial-scratch-message)))
+    (set-buffer-modified-p nil)))
+(global-set-key (kbd "C-c C-w h")
+		(lambda ()
+		  (interactive)
+		  (custom-split-window-and-new-scratch 'right)))
+(global-set-key (kbd "C-c C-w v")
+		(lambda ()
+		  (interactive)
+		  (custom-split-window-and-new-scratch 'below)))
+
 (provide 'kbd)
